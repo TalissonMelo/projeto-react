3c9afb5b789a433dab2a2df833161316a47c043e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-02-21 07:27:14 -0300

    Implementando Bakend JSON Server

--- src/components/personagens/PersonagemCRUD.jsx
+++ src/components/personagens/PersonagemCRUD.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
 const headerProps = {
@@ -9,12 +10,42 @@ const headerProps = {
 
 }
 
+const baseUrl = "http://localhost:3001/personagem";
+const initialState = {
+    personagem : { nome : ' ' , descricao : ' '},
+    list: []
+}
+
 export default class PersonagemCRUD extends Component {
+
+    state = {...initialState}
+
+    clear() {
+        this.setState({ personagem: initialState.personagem })
+    }
+
+    save() {
+        const personagem = this.state.personagem
+        const method = personagem.id ? 'put' : 'post'
+        const url = personagem.id ? `${baseUrl}/${personagem.id}` : baseUrl 
+        axios[method](url, personagem)
+            .then(resp => {
+                const list = this.getUpdateList(resp.data)
+                this.setState({personagem : initialState.personagem, list})
+            })
+    }
+
     render(){
         return(
             <Main {...headerProps}>
                 Cadastro de Personagem
             </Main>
         )
+    }
+
+    getUpdateList(personagem){
+        const list = this.state.list.filter(x => x.id !== personagem.id)
+        list.unshift(personagem)
+        return list;
     }
 }
