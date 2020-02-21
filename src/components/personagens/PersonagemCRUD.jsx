2521837d43743e476cf40cd04fa04b83cadb5f35
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-02-21 09:15:30 -0300

    Listando Personagens

--- src/components/personagens/PersonagemCRUD.jsx
+++ src/components/personagens/PersonagemCRUD.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component, useReducer } from 'react'
 import axios from 'axios'
 import Main from '../template/Main'
 
@@ -12,13 +12,19 @@ const headerProps = {
 
 const baseUrl = "http://localhost:3001/personagem";
 const initialState = {
-    personagem : { nome : ' ' , descricao : ' '},
+    personagem: { nome: '', descricao: '' },
     list: []
 }
 
 export default class PersonagemCRUD extends Component {
 
-    state = {...initialState}
+    state = { ...initialState }
+
+    componentList() {
+        axios(baseUrl).then(resp => {
+            this.setState({ list: resp.data})
+        })
+    }
 
     clear() {
         this.setState({ personagem: initialState.personagem })
@@ -27,31 +33,31 @@ export default class PersonagemCRUD extends Component {
     save() {
         const personagem = this.state.personagem
         const method = personagem.id ? 'put' : 'post'
-        const url = personagem.id ? `${baseUrl}/${personagem.id}` : baseUrl 
+        const url = personagem.id ? `${baseUrl}/${personagem.id}` : baseUrl
         axios[method](url, personagem)
             .then(resp => {
                 const list = this.getUpdateList(resp.data)
-                this.setState({personagem : initialState.personagem, list})
+                this.setState({ personagem: initialState.personagem, list })
             })
     }
 
-    renderForm(){
+    renderForm() {
         return (
             <div className="form">
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <div className="form-group">
-                                <label>Nome</label>
-                                <input type="text" className="form-control" name="nome" value={this.state.personagem.nome}
-                                onChange={e => this.updateField(e)} placeholder= "Digite o nome ..."></input>
+                            <label>Nome</label>
+                            <input type="text" className="form-control" name="nome" value={this.state.personagem.nome}
+                                onChange={e => this.updateField(e)}></input>
                         </div>
                     </div>
 
                     <div className="col-12 col-md-6">
                         <div className="form-group">
-                                <label>Descrição</label>
-                                <input type="text" className="form-control" name="descricao" value={this.state.personagem.descricao}
-                                onChange={e => this.updateField(e)} placeholder= "Digite a descrição do personagem ..."></input>
+                            <label>Descrição</label>
+                            <input type="text" className="form-control" name="descricao" value={this.state.personagem.descricao}
+                                onChange={e => this.updateField(e)}></input>
                         </div>
                     </div>
                 </div>
@@ -59,12 +65,12 @@ export default class PersonagemCRUD extends Component {
                 <div className="row">
                     <div className="col-12 d-flex justify-contend-end">
                         <button className="btn btn-primary"
-                            onClick={e => this.save(e)}>  
-                                Salvar
+                            onClick={e => this.save(e)}>
+                            Salvar
                         </button>
                         <button className="btn btn-secondary ml-2"
-                            onClick={e => this.clear(e)}>  
-                                Cancelar
+                            onClick={e => this.clear(e)}>
+                            Cancelar
                         </button>
                     </div>
                 </div>
@@ -72,22 +78,74 @@ export default class PersonagemCRUD extends Component {
         )
     }
 
-    updateField(event){
-        const personagem = {...this.state.user}
-        personagem[event.target.nome] = event.target.value 
+    updateField(event) {
+        const personagem = { ...this.state.personagem }
+        personagem[event.target.name] = event.target.value
         this.setState({ personagem })
     }
 
-    getUpdateList(personagem){
+    getUpdateList(personagem, add = true) {
         const list = this.state.list.filter(x => x.id !== personagem.id)
-        list.unshift(personagem)
+        if(add) list.unshift(personagem)
         return list;
     }
 
-    render(){
-        return(
+    load(personagem) {
+        this.setState({ personagem })
+    }
+
+    remove(personagem) {
+        axios.delete(`${baseUrl}/${personagem.id}`).then(resp => {
+            const list = this.getUpdateList(personagem, false)
+            this.setState({ list })
+        })
+    }
+
+    renderTable() {
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRow()}
+                </tbody>
+            </table>
+        )
+    }
+
+    renderRow() {
+        return this.state.list.map(personagem => {
+            return (
+                <tr key={personagem.id}>
+                    <td>{personagem.nome}</td>
+                    <td>{personagem.descricao}</td>
+                    <td>
+                        <button className="btn btn-warning"
+                            onClick={() => this.load(personagem)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+
+                        <button className="btn btn-danger ml-2"
+                            onClick={() => this.remove(personagem)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
+        console.log(this.state.list);
+        return (
             <Main {...headerProps}>
                 {this.renderForm()}
+                {this.renderTable()}
             </Main>
         )
     }
